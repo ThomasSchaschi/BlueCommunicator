88a0d0baa634eb024fdcb405edc4b71f32e9b477
ThomasSchaschi <sch17042@spengergasse.at>
2016-06-14 17:23:55 +0200

    Bluetooth Connection to one of paired devices.

--- app/src/main/java/com/usbcommunicator/thomas/bluecommunicator/Startup.java
+++ app/src/main/java/com/usbcommunicator/thomas/bluecommunicator/Startup.java
@@ -1,14 +1,18 @@
 package com.usbcommunicator.thomas.bluecommunicator;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import io.palaima.smoothbluetooth.Device;
@@ -34,6 +38,13 @@ public class Startup extends AppCompatActivity {
 
     public Context startUpContex;
 
+    public HashMap<String, Device> deviceAssortment;
+
+
+    //Selected item identification from listview
+    private String lastItem = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +53,14 @@ public class Startup extends AppCompatActivity {
 
         startUpContex = this;
         deviceView = (ListView)findViewById(R.id.list_item_deviceList);
+        setViewItemListener();
+
+        btnConnect = (Button)findViewById(R.id.btnConnect);
+        btnDisconnect = (Button)findViewById(R.id.btnDisconnect);
 
         smoothBluetooth = new SmoothBluetooth(this);
         smoothBluetooth.setListener(mListener);
         smoothBluetooth.tryConnection();
-
-
-        btnConnect = (Button)findViewById(R.id.btnConnect);
-        btnDisconnect = (Button)findViewById(R.id.btnDisconnect);
 
 
         globalDeviceArrayList = new String[10];
@@ -59,7 +70,23 @@ public class Startup extends AppCompatActivity {
         //deviceView.setAdapter(adaper);
 
 
+        //Finished Initialization
+
+
     }
+
+    //Setup Listener for Item to choose from
+    private void setViewItemListener(){
+        deviceView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemFromList = (String)deviceView.getItemAtPosition(position);
+                Log.i(TAG, "ReceivedItem : " +  selectedItemFromList);
+                lastItem = selectedItemFromList;
+            }
+        });
+    }
+
 
     private SmoothBluetooth.Listener mListener = new SmoothBluetooth.Listener() {
         @Override
@@ -75,11 +102,15 @@ public class Startup extends AppCompatActivity {
         @Override
         public void onConnecting(Device device) {
             //called when connecting to particular device
+            Log.i(TAG, "Connection attempt to device : " + device.getName());
         }
 
         @Override
         public void onConnected(Device device) {
             //called when connected to particular device
+            Log.i(TAG, "Connection succeeded to device : " + device.getName());
+
+
         }
 
         @Override
@@ -90,6 +121,7 @@ public class Startup extends AppCompatActivity {
         @Override
         public void onConnectionFailed(Device device) {
             //called when connection failed to particular device
+            Log.i(TAG, "Connection failed to device : " + device.getName());
         }
 
         @Override
@@ -115,13 +147,14 @@ public class Startup extends AppCompatActivity {
 
 
             globalDeviceArrayList = new String[deviceList.size()];
+            deviceAssortment = new HashMap<>();
+
             //ArrayList<String> stringDeviceList = new ArrayList<String>();
             for(int i = 0; i < deviceList.size(); i++){
                 Log.i(TAG, "Device : " + deviceList.get(i).getName());
                 globalDeviceArrayList[i] = deviceList.get(i).getName();
+                deviceAssortment.put(deviceList.get(i).getName(), deviceList.get(i));
             }
-
-
 
             //String[] mobileArray = (String[])stringDeviceList.toArray();
             //String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
@@ -130,7 +163,34 @@ public class Startup extends AppCompatActivity {
             adaper = new ArrayAdapter<String>(startUpContex, R.layout.listitem, globalDeviceArrayList);
             deviceView.setAdapter(adaper);
 
+
+            final SmoothBluetooth.ConnectionCallback finalConnectionCallback = connectionCallback;
+            btnConnect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //OnClick of btnConnect
+                    Log.i(TAG, "btnConnect");
+
+                    if(!lastItem.equals("")){
+                        Log.i(TAG, "Attempting connection to device : " + lastItem);
+                        //connectionCallback.connectTo(deviceAssortment.get(lastItem));
+                        connectToDevice(deviceAssortment.get(lastItem), finalConnectionCallback);
+                        lastItem = "";
+                    }else{
+                        Log.i(TAG, "No item selected.");
+                    }
+                }
+            });
+
+
+
+            connectionCallback = connectionCallback;
         }
+
+        public void connectToDevice(Device device, SmoothBluetooth.ConnectionCallback callback){
+            callback.connectTo(device);
+        }
+
 
         @Override
         public void onDataReceived(int data) {
@@ -138,6 +198,23 @@ public class Startup extends AppCompatActivity {
 
         }
     };
+
+    /**
+     * Button Listener to handle connect / disconnect commands
+     */
+    public View.OnClickListener genericButtonListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            final String TAG = "GenericButtonListener - ";
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        smoothBluetooth.stop();
+    }
 }
 
 
